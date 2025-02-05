36b9a1baa96a0486d1f4559b136870c46d8b6a46
Matheus Nicolas <manijrprof@gmail.com>
2025-02-05 16:08:15 -0300

    Atualização do execercicio da aula 2 e 4

--- ExerciciosAula/Aula16/Aula16ex002.py
+++ ExerciciosAula/Aula16/Aula16ex002.py
@@ -1,15 +1,22 @@
 '''Crie uma tupla preenchida com os 20 primeiros colocados da tabela do Campeonato Brasileiro de Futebol, na ordem de colocação. Depois mostre:
-A) Apenas os 5 primeiros colocados.
-B) Os últimos 4 colocados da tabela.
-C) Uma lista com os times em ordem alfabética.
-D) Em que posição na tabela está o time da Fluminense.'''
+A) Apenas os 5 primeiros colocados. OK
+B) Os últimos 4 colocados da tabela. OK
+C) Uma lista com os times em ordem alfabética. OK
+D) Em que posição na tabela está o time da Fluminense. OK'''
 
 brasileiro = ('Fortaleza', 'Botafogo', 'Palmeiras', 'Flamengo', 'São Paulo', 'Bahia', 'Cruzeiro', 
               'Atlético-MG', 'Athelico Paranaense', 'Vasco', 'Internacional', 'Juventude', 'Grêmio', 
               'Bragantino', 'Criciúma', 'Fluminense', 'Vitória', 'Corinthians', 'Cuiabá', 'Atlético-GO')
 
-print(f'Os primeiros 5 colocados são: {0:5}')
-print(f'A lista de times em ordem alfabética:')
+lista_alfabetica = []
+
+print(f'Os primeiros 5 colocados são: {brasileiro[0:5]}')
+
+print(f'Os 4 útlimos colocados são: {brasileiro[-4:]}')
+
 for time in brasileiro:
-    print(f'{time}')
+    lista_alfabetica.append(time)
+
+print(f'A lista de times em ordem alfabética: {sorted(lista_alfabetica)}')
+
 print(f'O Fluminense esta na posição {brasileiro.index('Fluminense') + 1}')
--- ExerciciosAula/Aula16/Aula16ex004.py
+++ ExerciciosAula/Aula16/Aula16ex004.py
@@ -10,10 +10,11 @@ n4 = int(input("INSIRA UM VALOR: "))
 
 qt_valor = 0
 posicao = 0
+valores_pares = []
 
 valores = (n1, n2, n3, n4)
 
-for valor in enumerate(valores):
+for valor in valores:
     if valor == 9:
         qt_valor += 1
 
@@ -23,11 +24,27 @@ for cont in range(0, len(valores)):
 
 print(f'Você digitou os valores {valores}')
 print(f'O valor 9 apareceu {qt_valor} vezes')
+
 if posicao != 0:
-    print(f'O valor 3 apareceu na {posicao}º posição')
+    print(f'O valor 3 apareceu pela primeira vez na {posicao}º posição')
 else:
     print(f'O valor 3 não foi digitado em nenhuma posição')
-print(f'Os valores pares digitados foram', end=' ')
+
+
 for valor in valores:
     if valor % 2 == 0:
-        print(valor, end=' ')
+        valores_pares.append(valor)
+
+qt_valores_pares = len(valores_pares) - 1
+
+
+if len(valores_pares) != 0:
+    print(f'Os valores pares digitados foram', end=' ')
+    
+    for valor in valores_pares:
+        if valor != valores_pares[qt_valores_pares]:
+            print(valor, end=', ')
+        else:
+            print(valor, end='.')
+else:
+    print(f'Foram digitados 0 valores pares')
