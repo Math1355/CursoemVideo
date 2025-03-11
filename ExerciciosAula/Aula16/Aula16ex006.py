2074a4d7c96d32aa08d9c30b7159af23c589c4fa
Matheus Nicolas <manijrprof@gmail.com>
2025-03-11 20:53:15 -0300

    Exercicio 6 da aula 16 feito

--- ExerciciosAula/Aula16/Aula16ex006.py
+++ ExerciciosAula/Aula16/Aula16ex006.py
@@ -4,3 +4,15 @@ Crie um programa que tenha uma tupla com várias palavras
 cada palavra, quais são as suas vogais, exemplo:
 """
 
+palavras = ("aprender", "programar", "linguagem", "python"
+            , "curso", "gratis", "estudar", "praticar"
+            , "trabalhar", "mercado", "programador", "futuro")
+
+vogais = ["a", "e", "i", "o", "u"]
+
+for palavra in palavras:
+    print(f"\nNa palavra {palavra.upper()} temos ", end= "")
+
+    for letra in palavra:
+        if letra in vogais:
+            print(letra, end=" ")
